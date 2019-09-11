04c54655bf4ec20d60e821017e041556160a91d2
SozinSol <32362639+SozinSol@users.noreply.github.com>
2019-09-11 17:57:28 -0400

    Update and rename lauren to madlib.java

---
+++ madlib.java
@@ -0,0 +1,80 @@
+package madlib;
+
+import java.util.Scanner;
+
+public class laurenSucksDick {
+	static Scanner dave = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Enter a name");
+		
+		String name = dave.nextLine();
+		
+		System.out.println("Enter a name again");
+		
+		String name2 = dave.nextLine();
+		
+		System.out.println("Enter a adverb");
+		
+		String adverb = dave.nextLine();
+		
+		System.out.println("Enter a adverb again");
+		
+		String adverb2 =dave.nextLine();
+		
+		System.out.println("Enter a adverb again");
+		
+		String adverb3 = dave.nextLine();
+		
+		System.out.println("Enter a verb");
+		
+		String verb = dave.nextLine();
+		
+		System.out.println("Enter a verb again");
+		
+		String verb2= dave.nextLine();
+		
+		System.out.println("Enter a verb ending in ed");
+		
+		String verbed = dave.nextLine();
+		
+		System.out.println("Enter a adjective ending in ed");
+		
+		String adjective = dave.nextLine();
+		
+		System.out.println("Enter a adjective again");
+		
+		String adjective2 = dave.nextLine();
+		
+		System.out.println("Enter a adjective again");
+		
+		String adjective3 = dave.nextLine();
+		
+		System.out.println("Enter a body part");
+		
+		String bodypart = dave.nextLine();
+		
+		System.out.println("Enter a body part again");
+		
+		String bodypart2 = dave.nextLine();
+		
+		System.out.println("Enter a noun");
+		
+		String noun = dave.nextLine();
+
+		
+		
+		
+		System.out.println("An infraction at school");
+		System.out.println("This is a incident report for" + " " + name ); 
+		System.out.println(" It was a normal day in the classroom when another student by the name of" + " " + name2 + " " + "bumped into them causing" + " " + name + " " +"to get aggravated and" + " " + adverb + " " + name2 + " " + "in the " + " " + bodypart);
+		System.out.println(" causing him to" + " " + verb + " " + ". when i tried to break them up" + " " + name + " " + "called me a  " + " " + adjective2 + " " + noun + " " + "hearing this caused me to" + " " + verb2 + " " +"and the entire calss was " + " " + adjective);
+		System.out.println("I then called an administrator but they" + " " + adverb3 + " " + "slipped on a " + " " + noun + " " + " causing them to hit their " + " " + bodypart2 + " " + ". Since the administrator was" + " " + adjective3 + " " + name + " " + " was " + " " + adverb2 + " " + verbed + " " + " on site.");
+		
+		
+	}
+
+}
