2169b051f6ea96f006de59ba8ac94441f47e161a
SozinSol <32362639+SozinSol@users.noreply.github.com>
2019-09-13 11:44:05 -0400

    Update madlib.java

--- madlib.java
+++ madlib.java
@@ -1,5 +1,3 @@
-package madlib;
-
 import java.util.Scanner;
 
 public class laurenSucksDick {
@@ -17,11 +15,11 @@ public class laurenSucksDick {
 		
 		String name2 = dave.nextLine();
 		
+		System.out.println("Enter a action");
+		
+		String action = dave.nextLine();
+		
 		System.out.println("Enter a adverb");
-		
-		String adverb = dave.nextLine();
-		
-		System.out.println("Enter a adverb again");
 		
 		String adverb2 =dave.nextLine();
 		
@@ -41,11 +39,11 @@ public class laurenSucksDick {
 		
 		String verbed = dave.nextLine();
 		
-		System.out.println("Enter a adjective ending in ed");
+		System.out.println("Enter a verb ending in ed");
 		
 		String adjective = dave.nextLine();
 		
-		System.out.println("Enter a adjective again");
+		System.out.println("Enter a adjective");
 		
 		String adjective2 = dave.nextLine();
 		
@@ -64,17 +62,21 @@ public class laurenSucksDick {
 		System.out.println("Enter a noun");
 		
 		String noun = dave.nextLine();
+		
+System.out.println("Enter a noun again");
+		
+		String noun2 = dave.nextLine();
 
-		
-		
 		
 		System.out.println("An infraction at school");
 		System.out.println("This is a incident report for" + " " + name ); 
-		System.out.println(" It was a normal day in the classroom when another student by the name of" + " " + name2 + " " + "bumped into them causing" + " " + name + " " +"to get aggravated and" + " " + adverb + " " + name2 + " " + "in the " + " " + bodypart);
-		System.out.println(" causing him to" + " " + verb + " " + ". when i tried to break them up" + " " + name + " " + "called me a  " + " " + adjective2 + " " + noun + " " + "hearing this caused me to" + " " + verb2 + " " +"and the entire calss was " + " " + adjective);
-		System.out.println("I then called an administrator but they" + " " + adverb3 + " " + "slipped on a " + " " + noun + " " + " causing them to hit their " + " " + bodypart2 + " " + ". Since the administrator was" + " " + adjective3 + " " + name + " " + " was " + " " + adverb2 + " " + verbed + " " + " on site.");
+		System.out.println("It was a normal day in the classroom when another student by the name of" + " " + name2 + " " + "bumped into them causing" + " " + name + " " +"to get aggravated and" + " " + action + " " + name2 + " " + "in the " + " " + bodypart);
+		System.out.println(",causing them to" + " " + verb + " " + ". When I tried to break them up" + " " + name + " " + "called me a  " + " " + adjective2 + " " + noun + " " + "hearing this caused me to" + " " + verb2 + " " +"and the entire class was " + " " + adjective);
+		System.out.println("I then called an administrator but they" + " " + adverb3 + " " + "slipped on a " + " " + noun2 + " " + " causing them to hit their " + " " + bodypart2);		
+		System.out.println(". Since the administrator was" + " " + adjective3 + "," + name + " " + " was " + " " + adverb2 + " " + verbed + " " + " on site.");
+	
 		
 		
 	}
-
+ 
 }
