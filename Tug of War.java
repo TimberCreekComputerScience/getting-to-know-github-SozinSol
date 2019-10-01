83377c57e17120427aeafd5a584c201d2f352bc8
SozinSol <32362639+SozinSol@users.noreply.github.com>
2019-10-01 17:25:57 -0400

    Update and rename Meth.java to Tug of War.java

---
+++ Tug of War.java
@@ -0,0 +1,44 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+
+public class Meth extends JFrame implements KeyListener {
+
+
+	public static void main(String[] args) {
+
+		new Meth().setVisible(true);
+	}
+
+	private Meth() {
+
+		super("Tug Of War");
+		setSize(600, 600);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	
+		
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
