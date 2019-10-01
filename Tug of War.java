4531233c76a3767d0fda22e7ab16e95aca0191d8
SozinSol <32362639+SozinSol@users.noreply.github.com>
2019-10-01 17:48:10 -0400

    Update Tug of War.java

--- Tug of War.java
+++ Tug of War.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -6,8 +5,7 @@ import javax.swing.JFrame;
 
 public class Meth extends JFrame implements KeyListener {
 
-	private mainDraw draw;
-	
+
 	public static void main(String[] args) {
 
 		new Meth().setVisible(true);
@@ -20,30 +18,26 @@ public class Meth extends JFrame implements KeyListener {
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+
+
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		System.out.println("keyPressed");
+		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
-			((mainDraw) draw).moveRight();
-		else if (e.getKeyCode() == KeyEvent.VK_LEFT)
-			draw.moveLeft();
-		else if (e.getKeyCode() == KeyEvent.VK_DOWN)
-			draw.moveDown();
-		else if (e.getKeyCode() == KeyEvent.VK_UP)
-			draw.moveUp();
+		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		System.out.println("keyTyped");
+		// TODO Auto-generated method stub
 
 	}
 
