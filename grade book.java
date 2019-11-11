d75c0a7b628e97b2610711b0eee58d8d6680ddd6
SozinSol <32362639+SozinSol@users.noreply.github.com>
2019-11-11 10:02:40 -0500

    Update grade book.java

--- grade book.java
+++ grade book.java
@@ -16,7 +16,7 @@ public class GradeBook {
 					+"2) Add New Assingnment\n"
 					+"3) Print Roster\n"
 					+"4) Drop Lowest Grade\n"
-					+"5) Exit\n");
+					+"5) Exit");
 			
 			option = scan.nextInt();
 			scan.nextLine();
@@ -29,14 +29,21 @@ public class GradeBook {
 			}else if ( option == 2){
 				System.out.println("Mass Assign Grade:");
 				int grade = scan.nextInt();
-				classRoster.add(new S)
+				int grade = scan.nextInt();
+				for (int = 0; i< classRoster.size(); i++)
+				{
+					classRoster.get(i).addAssignment(grade);
+				}
 			}else if (option == 3){
 				for (int i =0; i < classRoster.size(); i++){
 					System.out.println(classRoster.get(i).toString());
 				}
 				System.out.println();
+				System.out.println();
 			}else if (option == 4){
-				
+				for (int i = 0; i < classRoster.size(); i++){
+					classRoster.get(i).dropLowest();
+				}
 				
 				
 			}
@@ -50,3 +57,45 @@ public class GradeBook {
 	}
 
 }
+
+
+
+
+
+
+//differnt class
+
+public class Student {
+	private String name;
+	private int grade;
+	private ArrayList<Integer> assgignments;
+	private double average;
+	
+	public static void main(String n, int g) {
+	
+		name = n;
+		grade = g;
+		assignemnts = new ArrayList<Integer>();
+
+	}
+	public int getNumberOfAssignemnts() {
+		return assignments.size();
+	}
+	
+	public String getName(){
+		return name;
+	}
+	public int getGrade(){
+		return grade;
+	}
+	public String getScore(){
+		String answer = "" + average;
+		if (average == -1){
+			answer = no grades Gieegabdfm,bna
+		}
+	}
+
+}
+
+
+
