bda7c61adca22df16f173aefe095e9379efb4c58
Christian <christianlalchan@gmail.com>
2019-12-05 19:18:37 -0500

    okden

---
+++ Advent Of Code/src/dayOne.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class dayOne {
+	static Scanner dave = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		int mass = dave.nextInt();
+		int module = 0;
+		module = (mass/3)-2;
+		System.out.println(Math.floor(module));
+		
+		
+		
+	}
+
+}
