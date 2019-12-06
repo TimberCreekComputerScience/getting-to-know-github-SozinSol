c06f7707c3aabc266cf1f8a55361c8062976fefa
SozinSol <32362639+SozinSol@users.noreply.github.com>
2019-12-06 10:01:03 -0500

    Update Automobile.java

--- Automobliles/src/Automobile.java
+++ Automobliles/src/Automobile.java
@@ -1,22 +1,36 @@
- 
+
 public abstract class Automobile {
 
-	private int numOfWheels; 
+	private int numOfWheels;
 	private double fuelLevel;
 	private double maxFuel;
 	private int milesPerGallon;
-	
-	public Automobile(int a,int c, double b) {
-		numOfWheels = a;
-		fuelLevel =b;
-		milesPerGallon = c;
-	}
-	
-	public boolean isEmpty() {
-		return false;
-		
+
+	public Automobile(int wheels, int miles, double fuel) {
+		numOfWheels = wheels;
+		maxFuel = fuel;
+		fuelLevel = fuel;
+		milesPerGallon = miles;
 	}
 
-	
+	public void drive() {
+
+		fuelLevel--;
+
+	}
+
+	public boolean isEmpty(){
+		return true;
+	}
+
+	public void fillerUp() {
+		fuelLevel = maxFuel;
+	}
+
+	public String toString() {
+		
+		
+		return "";
+	}
 
 }
