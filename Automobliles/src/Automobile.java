5c1a285266f2ae682884aec569ef11710ec1e99c
Christian <christianlalchan@gmail.com>
2019-12-09 16:17:18 -0500

    ngfd

--- Automobliles/src/Automobile.java
+++ Automobliles/src/Automobile.java
@@ -4,16 +4,16 @@ public abstract class Automobile {
 	private int numOfWheels;
 	private double fuelLevel;
 	private double maxFuel;
-	private int milesPerGallon;
+	private double milesPerGallon;
 
-	public Automobile(int wheels, int miles, double fuel) {
+	public Automobile(int wheels, int miles, int fuel) {
 		numOfWheels = wheels;
 		maxFuel = fuel;
 		fuelLevel = fuel;
 		milesPerGallon = miles;
 	}
 
-	public void drive() {
+	public void drive(double drove) {
 
 		fuelLevel--;
 
