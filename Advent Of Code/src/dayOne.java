7e6ff941177da9776cc2a3012ed7d73ff778f9ba
Christian <christianlalchan@gmail.com>
2019-12-09 16:17:02 -0500

    foo

--- Advent Of Code/src/dayOne.java
+++ Advent Of Code/src/dayOne.java
@@ -4,13 +4,13 @@ public class dayOne {
 	static Scanner dave = new Scanner(System.in);
 
 	public static void main(String[] args) {
-
+for ( ; ; ) {
 		int mass = dave.nextInt();
 		int module = 0;
 		module = (mass/3)-2;
 		System.out.println(Math.floor(module));
 		
-		
+}
 		
 	}
 
---
+++ Advent Of Code/src/dayTwo.java
@@ -0,0 +1,9 @@
+
+public class dayTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
